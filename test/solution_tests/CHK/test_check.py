98b70267baec2490ff89c83fa47784dc26e29d8e
sbx_user1051 <sbx_user1051@169.254.10.69>
2023-06-11 12:36:51 +0000

    Sun Jun 11 12:36:51 UTC 2023

--- test/solution_tests/CHK/test_check.py
+++ test/solution_tests/CHK/test_check.py
@@ -21,5 +21,46 @@ class TestCheck():
     def test_special_offer(self):
         assert checkout_solution.checkout('AAA') == 130
 
+    def test_best_special_offer_used(self):
+        """
+        When multiple special offers are available, expect the one for the highest number
+        of items to be applied first
+        """
+        # Applies the discount for 5, not 3
+        assert checkout_solution.checkout(5 * 'A') == 200
+
     def test_special_offer_hits_multiple_times(self):
-        assert checkout_solution.checkout('AAAAAAA') == 310
+        # Expect the offer to be applied as many times as it can, with any excess
+        # priced normally
+        assert checkout_solution.checkout(5 * 'B') == 120
+
+    def test_order_of_special_offer_application(self):
+        """
+        Suppose we have two special offers for item 'A':
+         - 3 for 130
+         - 5 for 200
+
+         With a normal price of 1 for 50
+
+         Given 6 As, we should expect this to be applied as:
+           - 5 for 200
+           - 1 for 50
+         Which is 250
+
+         I.e. it does not work this out as 2 * (3 for 130) == 260
+
+         It maximises the number of times the largest discount can be used. Implicitly assumes therefore that discounts
+         on larger numbers of items are 'better'
+        """
+        assert checkout_solution.checkout(6 * 'A') == 250
+        # Hit 5 for 200 multiple times - should still prioritise this over 3 for 130
+        assert checkout_solution.checkout(11 * 'A') == 450
+
+    def test_multiple_discounts_hit(self):
+        """
+        Multiple different special offers can be applied to the same item, with the best discounts being applied first
+        until there are not enough items left, then the remainder being tested against smaller special offers and so on
+        """
+        # 5A for 200 + 3A for 130 == 330
+        assert checkout_solution.checkout(8 * 'A') == 330
+
