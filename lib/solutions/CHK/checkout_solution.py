12bab16693108f59a09e62e4705718a3aa7f8bea
sbx_user1051 <sbx_user1051@169.254.10.69>
2023-06-11 12:07:27 +0000

    Sun Jun 11 12:07:27 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,30 @@
+from collections import defaultdict
 
+
+prices = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15
+}
+
+special_offers = {
+    'A': (3, 130),
+    'B': (2, 45)
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    # Unclear at this point if the string is AAA or something like 3A for all items
-    return -1
+    # String will have a letter for each occurrence of the item
+    counts_per_sku = defaultdict(int)
+    for sku in skus:
+        if sku in prices:
+            counts_per_sku[sku] += 1
+        else:
+            return -1
+
+    total = 0
+    for sku, count in counts_per_sku:
+        if special_offer := special_offers.get('SKU'):
+            total += (count / )
